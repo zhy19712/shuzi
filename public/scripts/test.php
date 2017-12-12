f5312e67cb7708db6e46902e1f8e7f8928c03950
zhangchuan <993090861@qq.com>
2017-12-12 17:09:48 +0800

    1

--- public/scripts/test.php
+++ public/scripts/test.php
@@ -30,8 +30,14 @@ $primaryKey = 'id';
 // parameter represents the DataTables column identifier. In this case simple
 // indexes
 $columns = array(//定义数据库中查看的字段与表格中的哪一列相对应
-    array( 'db' => 'username',  'dt' => 0 )
-
+    array( 'db' => 'id',  'dt' => 0 ),
+    array( 'db' => 'username',  'dt' => 1 ),
+    array( 'db' => 'portrait',  'dt' => 2 ),
+    array( 'db' => 'loginnum',  'dt' => 3 ),
+    array( 'db' => 'last_login_ip',  'dt' => 4 ),
+    array( 'db' => 'last_login_time',  'dt' => 5 ),
+    array( 'db' => 'real_name',  'dt' => 6 ),
+    array( 'db' => 'status',  'dt' => 7 )
 );
 
 // SQL server connection information数据库连接信息
