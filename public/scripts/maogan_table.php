943d1298439627fea950cfedc552346ef6889420
zhangchuan <993090861@qq.com>
2017-12-28 11:34:47 +0800

    1

--- public/scripts/maogan_table.php
+++ public/scripts/maogan_table.php
@@ -30,7 +30,7 @@ $primaryKey = 'id';
 // parameter represents the DataTables column identifier. In this case simple
 // indexes
 $columns = array(//定义数据库中查看的字段与表格中的哪一列相对应
-    array( 'db' => 'uid',  'dt' => 0 ),
+    array( 'db' => 'id',  'dt' => 0 ),
     array( 'db' => 'standard',  'dt' => 1 ),
 );
 
