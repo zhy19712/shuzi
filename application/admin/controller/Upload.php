7e25cbf2fb47c397313612a5a6af4c761278ab5c
wangguoxin <657826620@qq.com>
2017-12-28 14:57:42 +0800

    质量管理

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 use think\Controller;
 use think\File;
 use think\Request;
+use app\admin\model\ProjectAttachmentModel;
 
 class Upload extends Base
 {
@@ -31,10 +32,18 @@ class Upload extends Base
 
     //文件上传
     public function uploadfile(){
+        $attachment = new ProjectAttachmentModel();
         $file = request()->file('file');
         $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/attachment');
         if($info){
-            echo $info->getSaveName();
+            $param = [       //接受传递的参数
+                'owner' => session('username')
+
+            ];
+            $filename =  $info->getSaveName();
+            $username = session('username');
+
+
         }else{
             echo $file->getError();
         }
---
+++ application/admin/model/ProjectAttachmentModel.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/12/28
+ * Time: 14:35
+ */
+
+namespace app\admin\model;
+
+
+use think\Model;
+
+class ProjectAttachmentModel extends Model
+{
+    protected $name = 'project_attachment';
+    /**
+     * 插入
+     */
+    public function insertAttachment($param)
+    {
+        try{
+            $result = $this->allowField(true)->save($param);
+            if(false === $result){
+                return ['code' => -1, 'data' => '', 'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'data' => '', 'msg' => '文件添加成功'];
+            }
+        }catch( PDOException $e){
+            return ['code' => -2, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * 编辑信息
+     */
+    public function editAttachment($param)
+    {
+        try{
+            $result =  $this->allowField(true)->save($param, ['uid' => $param['uid']]);
+            if(false === $result){
+                return ['code' => 0, 'data' => '', 'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'data' => '', 'msg' => '文件编辑成功'];
+            }
+        }catch( PDOException $e){
+            return ['code' => 0, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+
+    /**
+     * 根据uid获取信息
+     * @param $uid
+     */
+    public function getOne($uid)
+    {
+        return $this->where('uid', $uid)->find();
+    }
+}
