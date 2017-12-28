d83febefb537136e501aa46aad2904b5a1679a64
zhangchuan <993090861@qq.com>
2017-12-28 10:24:58 +0800

    1

--- application/admin/model/ZhihuModel.php
+++ application/admin/model/ZhihuModel.php
@@ -37,7 +37,7 @@ class ZhihuModel extends Model
     public function edit($param)
     {
         try{
-            $result =  $this->allowField(true)->save($param, ['id' => $param['id']]);
+            $result =  $this->allowField(true)->save($param, ['uid' => $param['uid']]);
             if(false === $result){
                 return ['code' => 0, 'data' => '', 'msg' => $this->getError()];
             }else{
