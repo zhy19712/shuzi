938d23e6816a19e6c2d23fe256cdf199668fbccb
zhy19712 <18055089@qq.com>
2018-01-04 02:13:17 +0800

    level2qualitly

--- application/admin/controller/Qc.php
+++ application/admin/controller/Qc.php
@@ -7,8 +7,11 @@
  */
 
 namespace app\admin\controller;
+use app\admin\model\QCMemberModel;
 use app\admin\model\QCModel;
 use app\admin\model\QCAttachmentModel;
+use app\admin\model\QCProblemModel;
+use app\admin\model\QCStrategyModel;
 
 
 class Qc extends Base
@@ -52,6 +55,127 @@ class Qc extends Base
             return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
         }
     }
+
+    public function memberEdit()
+    {
+        $qc = new QCMemberModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            $data = $qc->getOne($param['id']);
+            return json(['data' => $data]);
+        }
+        return $this->fetch();
+    }
+
+    public function memberAdd()
+    {
+        $qc = new QCMemberModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            if(empty($param['id']))
+            {
+                $flag = $qc->insertQc($param);
+                return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+            }
+            else if(!empty($param['id']))
+            {
+                $flag = $qc->editQc($param);
+                return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+            }
+
+        }
+    }
+
+    public function memberDel()
+    {
+        $qc = new QCMemberModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            $flag = $qc->delQc($param['id']);
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+        }
+    }
+
+    public function problemEdit()
+    {
+        $qc = new QCProblemModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            $data = $qc->getOne($param['id']);
+            return json(['data' => $data]);
+        }
+        return $this->fetch();
+    }
+
+    public function problemAdd()
+    {
+        $qc = new QCProblemModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            if(empty($param['id']))
+            {
+                $flag = $qc->insertQc($param);
+                return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+            }
+            else if(!empty($param['id']))
+            {
+                $flag = $qc->editQc($param);
+                return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+            }
+
+        }
+    }
+
+    public function problemDel()
+    {
+        $qc = new QCProblemModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            $flag = $qc->delQc($param['id']);
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+        }
+    }
+
+    public function strategyEdit()
+    {
+        $qc = new QCStrategyModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            $data = $qc->getOne($param['id']);
+            return json(['data' => $data]);
+        }
+        return $this->fetch();
+    }
+
+    public function strategyAdd()
+    {
+        $qc = new QCStrategyModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            if(empty($param['id']))
+            {
+                $flag = $qc->insertQc($param);
+                return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+            }
+            else if(!empty($param['id']))
+            {
+                $flag = $qc->editQc($param);
+                return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+            }
+
+        }
+    }
+
+    public function strategyDel()
+    {
+        $qc = new QCStrategyModel();
+        if(request()->isAjax()){
+            $param = input('post.');
+            $flag = $qc->delQc($param['id']);
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+        }
+    }
+
 
     public function saveAttachmentInfo()
     {
