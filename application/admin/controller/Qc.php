7767be5585bc54aece6daa6b590a408f098f03b3
wangguoxin <657826620@qq.com>
2018-01-04 14:17:45 +0800

    质量管理

--- application/admin/controller/Qc.php
+++ application/admin/controller/Qc.php
@@ -208,8 +208,8 @@ class Qc extends Base
                 $flag = $attachment->insertAttachment($data);
                 return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
             }else{
-                $data_older = $attachment->getOne($param['id']);
-                unlink($data_older['path']); //删除原先的文件
+                $data_older = $attachment->getOne();
+                unlink($data_older['path']);
                 if($param['table_name'] == 'ss' || $param['table_name'] == 'smyxzl'){
                     $data = [
                         'owner' => session('username'),
@@ -218,7 +218,8 @@ class Qc extends Base
                         'name' => $param['name'],
                         'revision' => $param['revision'],
                         'group_id' => $param['group_id'],
-                        'table_name' => $param['table_name']
+                        'table_name' => $param['table_name'],
+                        'id' =>$param['id']
                     ];
                 }else{
                     $data = [
@@ -227,10 +228,12 @@ class Qc extends Base
                         'path' => $param['path'],
                         'name' => $param['name'],
                         'group_id' => $param['group_id'],
-                        'table_name' => $param['table_name']
+                        'table_name' => $param['table_name'],
+                        'id' =>$param['id']
                     ];
                 }
-                $flag = $attachment->editAttachment($param);
+
+                $flag = $attachment->editAttachment($data);
                 return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
             }
 
