8c406a0d5d843b90cde919cb72866d99501fb5b9
zhuangyf <18055089@qq.com>
2018-01-11 10:00:37 +0800

    标准工艺

--- public/scripts/prototype_list.php
+++ public/scripts/prototype_list.php
@@ -20,7 +20,7 @@
  */
 
 // DB table to use
-$table = 'think_prototype';
+$table = 'think_prototype_list';
 
 // Table's primary key
 $primaryKey = 'id';
@@ -31,11 +31,11 @@ $primaryKey = 'id';
 // indexes
 $columns = array(//定义数据库中查看的字段与表格中的哪一列相对应
     array( 'db' => 'id',  'dt' => 0 ),
-    array( 'db' => 'name',  'dt' => 1 ),
-    array( 'db' => 'year',  'dt' => 2 ),
-    array( 'db' => 'season',  'dt' => 3 ),
-    array( 'db' => 'owner',  'dt' => 4 ),
-    array( 'db' => 'date',  'dt' => 5 )
+    array( 'db' => 'part',  'dt' => 1 ),
+    array( 'db' => 'owner',  'dt' => 2 ),
+    array( 'db' => 'date',  'dt' => 3 ),
+    array( 'db' => 'remark',  'dt' => 4 ),
+    array( 'db' => 'status',  'dt' => 5 )
 );
 
 // SQL server connection information数据库连接信息
@@ -53,11 +53,18 @@ $sql_details = array(
 
 require( 'ssp.class.php' );
 
-
-echo json_encode(
-    SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns)
-);
-
+if(!empty($_GET["group_id"]))
+{
+    $group_id = $_GET["group_id"];
+    echo json_encode(
+        SSP::complex( $_GET, $sql_details, $table, $primaryKey, $columns, null, "group_id = '$group_id'" )
+    );
+}
+else{
+    echo json_encode(
+        SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns)
+    );
+}
 
 
 
