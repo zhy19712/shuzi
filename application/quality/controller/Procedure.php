9aebd3205aeb7f72ac0e449a1c14d4ea51707b15
wangguoxin <657826620@qq.com>
2018-01-22 16:59:02 +0800

    模块划分

--- application/quality/controller/Procedure.php
+++ application/quality/controller/Procedure.php
@@ -89,22 +89,27 @@ class Procedure extends Base
             $param = input('post.');
             $data = $attachment->getOne($param['id']);
             $path = $data['path'];
-            $extension = get_extension($path);
-            $pdf_path = '/uploads/temp/' . basename($path) . '.pdf';
+            $extension = get_extension(substr($path,1));
+            $pdf_path = './uploads/temp/' . basename($path) . '.pdf';
             if($extension == 'doc' || $extension == 'docx' || $extension == 'txt'){
                 doc_to_pdf($path);
             }else if($extension == 'xls' || $extension == 'xlsx'){
                 excel_to_pdf($path);
             }else if($extension == 'ppt' || 'pptx'){
                 ppt_to_pdf($path);
+            }else if($extension == 'pdf'){
+                $resutl = copy($path, $pdf_path);
+                if($resutl){
+                    return json(['code' => 1, 'path' => substr($pdf_path,1)]);
+                }
             }else{
                 return json(['code' => 0, 'path' => $pdf_path, 'msg' => '不支持的文件类型']);
             }
 
             if(file_exists($pdf_path)){
-                return json(['code' => 1, 'path' => $pdf_path]);
+                return json(['code' => 1, 'path' => substr($pdf_path,1)]);
             }else{
-                return json(['code' => 0, 'path' => $pdf_path, 'msg' => '文件预览失败']);
+                return json(['code' => 0, 'path' => substr($pdf_path,1), 'msg' => '文件预览失败']);
             }
         }
     }
