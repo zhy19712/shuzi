ceffc826e03433beacdbaa16e0a8efa9b4899095
huwtao <hutaosir@163.com>
2018-01-29 14:19:12 +0800

    safety commit

--- application/safety/controller/Upload.php
+++ application/safety/controller/Upload.php
@@ -4,6 +4,7 @@ namespace app\safety\controller;
 use app\admin\controller\Base;
 use app\safety\model\ResponsibilityModel;
 use app\safety\model\SafetyGoalAnualModel;
+use app\safety\model\SafetyGoalGeneralModel;
 
 class Upload extends Base
 {
@@ -13,7 +14,7 @@ class Upload extends Base
         $year = request()->param('year');
         $remark = request()->param('remark');
         $file = request()->file('file');
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/safety');
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/safety/responsibility');
         if($info){
             $temp = $info->getSaveName();
             $path = './uploads/acceptance/' . str_replace("\\","/",$temp);
@@ -40,7 +41,7 @@ class Upload extends Base
         $name = request()->param('name');
         $remark = request()->param('remark');
         $file = request()->file('file');
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/safety');
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/safety/anual');
         if($info){
             $temp = $info->getSaveName();
             $path = './uploads/acceptance/' . str_replace("\\","/",$temp);
@@ -54,6 +55,31 @@ class Upload extends Base
                 'remark' => $remark
             ];
             $flag = $responsibility->insertSafetyGoalAnual($data);
+            return json(['code' => $flag['code'],  'msg' => $flag['msg']]);
+        }else{
+            echo $file->getError();
+        }
+    }
+
+    public function uploadSafeyGoalGeneral(){
+        $responsibility = new SafetyGoalGeneralModel();
+        $name = request()->param('name');
+        $remark = request()->param('remark');
+        $file = request()->file('file');
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/safety/general');
+        if($info){
+            $temp = $info->getSaveName();
+            $path = './uploads/acceptance/' . str_replace("\\","/",$temp);
+            $filename = $file->getInfo('name');
+            $data = [
+                'name' => $name,
+                'filename' => $filename,
+                'owner' => session('username'),
+                'date' => date("Y-m-d H:i:s"),
+                'path' => $path,
+                'remark' => $remark
+            ];
+            $flag = $responsibility->insertSafetyGoalGeneral($data);
             return json(['code' => $flag['code'],  'msg' => $flag['msg']]);
         }else{
             echo $file->getError();
