94fc61ac0845697843b5cb7fc13702da11b71822
huwtao <hutaosir@163.com>
2018-01-29 17:13:02 +0800

    safety commit

--- application/safety/controller/Responsibility.php
+++ application/safety/controller/Responsibility.php
@@ -12,6 +12,7 @@ namespace app\safety\controller;
 use app\admin\controller\Base;
 use app\safety\model\ResponsibilityModel;
 use app\safety\model\SafetyGoalAnualModel;
+use app\safety\model\SafetyGoalGeneralModel;
 
 class Responsibility extends Base
 {
@@ -26,7 +27,7 @@ class Responsibility extends Base
         return $this->fetch();
     }
 
-    public function editAnual()
+    public function anualEdit()
     {
         $anual = new SafetyGoalAnualModel();
         $param = input('post.');
@@ -113,6 +114,136 @@ class Responsibility extends Base
             }else{
                 return json(['code' => $code,  'path' => substr($pdf_path,1), 'msg' => $msg]);
             }
+        }
+    }
+
+    public function postGeneral()
+    {
+        if(request()->isAjax()){
+            $general = new SafetyGoalGeneralModel();
+            $param = input('post.');
+            $data = $general->getOne($param['id']);
+            return json(['code'=> 1, 'data' => $data]);
+        }
+        return $this->fetch();
+    }
+
+    public function generalEdit()
+    {
+        $general = new SafetyGoalGeneralModel();
+        $param = input('post.');
+        if(request()->isAjax()){
+            $data = [
+                'name' => $param['name'],
+                'owner' => session('username'),
+                'date' => date("Y-m-d H:i:s"),
+                'year' => $param['year'],
+                'remark' => $param['remark']
+            ];
+            $flag = $general->editSafetyGoalGeneral($data);
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+        }
+    }
+
+    public function generalDownload()
+    {
+        if(request()->isAjax()){
+            return json(['code' => 1]);
+        }
+        $id = input('param.id');
+        $general = new SafetyGoalGeneralModel();
+        $param = $general->getOne($id);
+        $filePath = $param['path'];
+        $fileName = $param['name'] . '.' . substr(strrchr($filePath, '.'), 1);
+        $file = fopen($filePath, "r"); //   打开文件
+        //输入文件标签
+        $fileName = iconv("utf-8","gb2312",$fileName);
+        Header("Content-type:application/octet-stream ");
+        Header("Accept-Ranges:bytes ");
+        Header("Accept-Length:   " . filesize($filePath));
+        Header("Content-Disposition:   attachment;   filename= " . $fileName);
+
+        //   输出文件内容
+        echo fread($file, filesize($filePath));
+        fclose($file);
+        exit;
+    }
+
+    public function generalDel()
+    {
+        $general = new SafetyGoalGeneralModel();
+        if(request()->isAjax()) {
+            $param = input('post.');
+            $data = $general->getOne($param['id']);
+            $path = $data['path'];
+            $pdf_path = './uploads/temp/' . basename($path) . '.pdf';
+            if(file_exists($path)){
+                unlink($path); //删除文件
+            }
+            if(file_exists($pdf_path)){
+                unlink($pdf_path); //删除生成的预览pdf
+            }
+            $flag = $general->delSafetyGoalGeneral($param['id']);
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+        }
+    }
+
+    public function generalPreview()
+    {
+        $general = new SafetyGoalGeneralModel();
+        if(request()->isAjax()) {
+            $param = input('post.');
+            $code = 1;
+            $msg = '预览成功';
+            $data = $general->getOne($param['id']);
+            $path = $data['path'];
+            $extension = strtolower(get_extension(substr($path,1)));
+            $pdf_path = './uploads/temp/' . basename($path) . '.pdf';
+            if(!file_exists($pdf_path)){
+                if($extension === 'doc' || $extension === 'docx' || $extension === 'txt'){
+                    doc_to_pdf($path);
+                }else if($extension === 'xls' || $extension === 'xlsx'){
+                    excel_to_pdf($path);
+                }else if($extension === 'ppt' || $extension === 'pptx'){
+                    ppt_to_pdf($path);
+                }else if($extension === 'pdf'){
+                    $pdf_path = $path;
+                }else{
+                    $code = 0;
+                    $msg = '不支持的文件格式';
+                }
+                return json(['code' => $code, 'path' => substr($pdf_path,1), 'msg' => $msg]);
+            }else{
+                return json(['code' => $code,  'path' => substr($pdf_path,1), 'msg' => $msg]);
+            }
+        }
+    }
+
+    public function postResponsibility()
+    {
+        if(request()->isAjax()){
+            $responsibility= new ResponsibilityModel();
+            $param = input('post.');
+            $data = $responsibility->getOne($param['id']);
+            return json(['code'=> 1, 'data' => $data]);
+        }
+        return $this->fetch();
+    }
+
+    public function responsibilityEdit()
+    {
+        $responsibility = new ResponsibilityModel();
+        $param = input('post.');
+        if(request()->isAjax()){
+            $data = [
+                'name' => $param['name'],
+                'owner' => session('username'),
+                'date' => date("Y-m-d H:i:s"),
+                'dept' =>$param['dept'],
+                'remark' => $param['remark']
+            ];
+            $flag = $responsibility->editResponsibility($data);
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
         }
     }
 
