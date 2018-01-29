2965321405418b78d3bf087aea958a8d18557b6e
huwtao <hutaosir@163.com>
2018-01-29 11:50:20 +0800

    safety commit

---
+++ application/safety/controller/Upload.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\safety\controller;
+use app\admin\controller\Base;
+use app\safety\model\ResponsibilityModel;
+
+class Upload extends Base
+{
+    public function uploadResponsibility(){
+        $responsibility = new ResponsibilityModel();
+        $name = request()->param('name');
+        $year = request()->param('year');
+        $remark = request()->param('remark');
+        $file = request()->file('file');
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/safety');
+        if($info){
+            $temp = $info->getSaveName();
+            $path = './uploads/acceptance/' . str_replace("\\","/",$temp);
+            $filename = $file->getInfo('name');
+            $data = [
+                'name' => $name,
+                'filename' => $filename,
+                'owner' => session('username'),
+                'date' => date("Y-m-d H:i:s"),
+                'year' => $year,
+                'path' => $path,
+                'remark' => $remark,
+                'dept' => session('dept')
+            ];
+            $flag = $responsibility->insertResponsibility($data);
+            return json(['code' => $flag['code'],  'msg' => $flag['msg']]);
+        }else{
+            echo $file->getError();
+        }
+    }
+}
