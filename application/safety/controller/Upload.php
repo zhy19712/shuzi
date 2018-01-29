cabefa4dfac0c7a62e4c294d7d93156a99c8e94c
huwtao <hutaosir@163.com>
2018-01-29 13:51:32 +0800

    safety commit

--- application/safety/controller/Upload.php
+++ application/safety/controller/Upload.php
@@ -3,6 +3,7 @@
 namespace app\safety\controller;
 use app\admin\controller\Base;
 use app\safety\model\ResponsibilityModel;
+use app\safety\model\SafetyGoalAnualModel;
 
 class Upload extends Base
 {
@@ -28,6 +29,31 @@ class Upload extends Base
                 'dept' => session('dept')
             ];
             $flag = $responsibility->insertResponsibility($data);
+            return json(['code' => $flag['code'],  'msg' => $flag['msg']]);
+        }else{
+            echo $file->getError();
+        }
+    }
+
+    public function uploadSafeyGoalAnual(){
+        $responsibility = new SafetyGoalAnualModel();
+        $name = request()->param('name');
+        $remark = request()->param('remark');
+        $file = request()->file('file');
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads/safety');
+        if($info){
+            $temp = $info->getSaveName();
+            $path = './uploads/acceptance/' . str_replace("\\","/",$temp);
+            $filename = $file->getInfo('name');
+            $data = [
+                'name' => $name,
+                'filename' => $filename,
+                'owner' => session('username'),
+                'date' => date("Y-m-d H:i:s"),
+                'path' => $path,
+                'remark' => $remark
+            ];
+            $flag = $responsibility->insertSafetyGoalAnual($data);
             return json(['code' => $flag['code'],  'msg' => $flag['msg']]);
         }else{
             echo $file->getError();
---
+++ application/safety/model/SafetyGoalAnualModel.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/1/4
+ * Time: 15:37
+ */
+
+namespace app\safety\model;
+
+
+use think\exception\PDOException;
+use think\Model;
+
+class SafetyGoalAnualModel extends Model
+{
+    protected $name = 'safety_goal_anual';
+
+    public function insertSafetyGoalAnual($param)
+    {
+        try{
+            $result = $this->allowField(true)->save($param);
+            if(false === $result){
+                return ['code' => -1, 'data' => '', 'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'data' => '', 'msg' => '添加成功'];
+            }
+        }catch( PDOException $e){
+            return ['code' => -2, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    public function editSafetyGoalAnual($param)
+    {
+        try{
+            $result =  $this->allowField(true)->save($param, ['id' => $param['id']]);
+            if(false === $result){
+                return ['code' => 0, 'data' => '', 'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'data' => '', 'msg' => '编辑成功'];
+            }
+        }catch( PDOException $e){
+            return ['code' => 0, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    public function delSafetyGoalAnual($id)
+    {
+        try{
+            $this->where('id', $id)->delete();
+            return ['code' => 1, 'data' => '', 'msg' => '删除成功'];
+
+        }catch( PDOException $e){
+            return ['code' => 0, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    public function getOne($id)
+    {
+        return $this->where('id', $id)->find();
+    }
+}
