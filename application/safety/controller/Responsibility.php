d7866aab37a5f3b0d9e9f679e6720d7bc148c0dd
huwtao <hutaosir@163.com>
2018-01-29 16:42:20 +0800

    safety commit

--- application/safety/controller/Responsibility.php
+++ application/safety/controller/Responsibility.php
@@ -11,12 +11,109 @@ namespace app\safety\controller;
 
 use app\admin\controller\Base;
 use app\safety\model\ResponsibilityModel;
+use app\safety\model\SafetyGoalAnualModel;
 
 class Responsibility extends Base
 {
     public function index()
     {
+        if(request()->isAjax()){
+            $anual= new SafetyGoalAnualModel();
+            $param = input('post.');
+            $data = $anual->getOne($param['id']);
+            return json(['code'=> 1, 'data' => $data]);
+        }
         return $this->fetch();
+    }
+
+    public function editAnual()
+    {
+        $anual = new SafetyGoalAnualModel();
+        $param = input('post.');
+        if(request()->isAjax()){
+            $data = [
+                'name' => $param['name'],
+                'owner' => session('username'),
+                'date' => date("Y-m-d H:i:s"),
+                'remark' => $param['remark']
+            ];
+            $flag = $anual->editSafetyGoalAnual($data);
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+        }
+    }
+
+    public function anualDownload()
+    {
+        if(request()->isAjax()){
+            return json(['code' => 1]);
+        }
+        $id = input('param.id');
+        $anual = new SafetyGoalAnualModel();
+        $param = $anual->getOne($id);
+        $filePath = $param['path'];
+        $fileName = $param['name'] . '.' . substr(strrchr($filePath, '.'), 1);
+        $file = fopen($filePath, "r"); //   打开文件
+        //输入文件标签
+        $fileName = iconv("utf-8","gb2312",$fileName);
+        Header("Content-type:application/octet-stream ");
+        Header("Accept-Ranges:bytes ");
+        Header("Accept-Length:   " . filesize($filePath));
+        Header("Content-Disposition:   attachment;   filename= " . $fileName);
+
+        //   输出文件内容
+        echo fread($file, filesize($filePath));
+        fclose($file);
+        exit;
+    }
+
+    public function anualDel()
+    {
+        $anual = new SafetyGoalAnualModel();
+        if(request()->isAjax()) {
+            $param = input('post.');
+            $data = $anual->getOne($param['id']);
+            $path = $data['path'];
+            $pdf_path = './uploads/temp/' . basename($path) . '.pdf';
+            if(file_exists($path)){
+                unlink($path); //删除文件
+            }
+            if(file_exists($pdf_path)){
+                unlink($pdf_path); //删除生成的预览pdf
+            }
+            $flag = $anual->delSafetyGoalAnual($param['id']);
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+        }
+    }
+
+    public function anualPreview()
+    {
+        $anual = new SafetyGoalAnualModel();
+        if(request()->isAjax()) {
+            $param = input('post.');
+            $code = 1;
+            $msg = '预览成功';
+            $data = $anual->getOne($param['id']);
+            $path = $data['path'];
+            $extension = strtolower(get_extension(substr($path,1)));
+            $pdf_path = './uploads/temp/' . basename($path) . '.pdf';
+            if(!file_exists($pdf_path)){
+                if($extension === 'doc' || $extension === 'docx' || $extension === 'txt'){
+                    doc_to_pdf($path);
+                }else if($extension === 'xls' || $extension === 'xlsx'){
+                    excel_to_pdf($path);
+                }else if($extension === 'ppt' || $extension === 'pptx'){
+                    ppt_to_pdf($path);
+                }else if($extension === 'pdf'){
+                    $pdf_path = $path;
+                }else{
+                    $code = 0;
+                    $msg = '不支持的文件格式';
+                }
+                return json(['code' => $code, 'path' => substr($pdf_path,1), 'msg' => $msg]);
+            }else{
+                return json(['code' => $code,  'path' => substr($pdf_path,1), 'msg' => $msg]);
+            }
+        }
     }
 
     public function responsibilityDownload()
@@ -92,4 +189,7 @@ class Responsibility extends Base
             }
         }
     }
+
+
+
 }
