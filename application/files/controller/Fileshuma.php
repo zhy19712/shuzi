22b33ed9f93191c04b4aa2256d7312642164e816
huwtao <hutaosir@163.com>
2018-02-01 11:21:07 +0800

    safety commit

--- application/files/controller/Fileshuma.php
+++ application/files/controller/Fileshuma.php
@@ -9,10 +9,18 @@ namespace app\files\controller;
 
 
 use app\admin\controller\Base;
+use app\files\model\FileshumaModel;
+
 class Fileshuma extends Base
 {
     public function index()
     {
+        if(request()->isAjax()){
+            $shuma = new FileshumaModel();
+            $param = input('post.');
+            $data = $shuma->getOne($param['id']);
+            return json(['code'=> 1, 'data' => $data]);
+        }
         return $this->fetch();
     }
 }
