55da8f210a698f8469b0c5d2fb4980ca541d2470
wangguoxin <657826620@qq.com>
2018-03-07 11:01:00 +0800

    bug修复

--- application/safety/controller/Statutestdi.php
+++ application/safety/controller/Statutestdi.php
@@ -3,14 +3,14 @@
  * Created by PhpStorm.
  * User: admin
  * Date: 2018/3/7
- * Time: 9:49
+ * Time: 10:53
  */
-//机构和职责
+//法规标准识别
 namespace app\safety\controller;
 
 use app\admin\controller\Base;
 
-class Responsibilityinsty extends Base
+class Statutestdi extends Base
 {
     public  function  index()
     {
