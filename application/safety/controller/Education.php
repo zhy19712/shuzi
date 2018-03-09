7b7c6b4b56ebc4e936e99081ce3446213aabe8b8
huwtao <hutaosir@163.com>
2018-03-09 15:57:16 +0800

    safety education commit

---
+++ application/safety/controller/Education.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sir
+ * Date: 2018/3/9
+ * Time: 13:45
+ */
+
+namespace app\safety\controller;
+
+// 专题教育培训
+use app\admin\controller\Base;
+use app\safety\model\EducationModel;
+
+class Education extends Base
+{
+    public function index()
+    {
+        return $this ->fetch();
+    }
+
+    /**
+     * 新增或者修改
+     * @return \think\response\Json
+     * @author hutao
+     */
+    public function eduAdd()
+    {
+        if(request()->isAjax()){
+            $edu = new EducationModel();
+            $param = input('post.');
+            if(empty($param['id'])){
+                $param['owner'] = session('username');
+                $param['edu_date'] = date("Y-m-d H:i:s");
+                $flag = $edu->insertEdu($param);
+            }else{
+                $flag = $edu->editEdu($param);
+            }
+            return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
+        }
+    }
+
+    public function eudDel()
+    {
+
+    }
+
+    public function eduDownload()
+    {
+
+    }
+
+}
---
+++ application/safety/model/EducationModel.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sir
+ * Date: 2018/3/7
+ * Time: 14:01
+ */
+
+namespace app\safety\model;
+
+
+use think\exception\PDOException;
+use think\Model;
+
+class EducationModel extends Model
+{
+    protected $name = 'safety_education';
+
+    public function insertEdu($param)
+    {
+        try{
+            $result = $this->allowField(true)->save($param);
+            if(false === $result){
+                return ['code' => -1, 'data' => '', 'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'data' => '', 'msg' => '添加成功'];
+            }
+        }catch( PDOException $e){
+            return ['code' => -2, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    public function editEdu($param)
+    {
+        try{
+            $result =  $this->allowField(true)->save($param, ['id' => $param['id']]);
+            if(false === $result){
+                return ['code' => 0, 'data' => '', 'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'data' => '', 'msg' => '编辑成功'];
+            }
+        }catch( PDOException $e){
+            return ['code' => 0, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    public function delEdu($id)
+    {
+        try{
+            $this->where('id', $id)->delete();
+            return ['code' => 1, 'data' => '', 'msg' => '删除成功'];
+        }catch( PDOException $e){
+            return ['code' => 0, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    public function getOne($id)
+    {
+        return $this->where('id', $id)->find();
+    }
+}
