ad9822f90337c16126b6a7f86a84d2d57f2e4c1a
huwtao <hutaosir@163.com>
2018-03-12 15:50:50 +0800

    safety education commit

--- application/safety/model/StatutestdiModel.php
+++ application/safety/model/StatutestdiModel.php
@@ -84,4 +84,18 @@ class StatutestdiModel extends Model
         return ['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']];
     }
 
+    public function getList($idArr)
+    {
+        $data = [];
+        foreach($idArr as $v){
+            $data[] = $this->getOne($v);
+        }
+        return $data;
+    }
+
+    public function getYears()
+    {
+        return $this->group('years')->column('years');
+    }
+
 }
