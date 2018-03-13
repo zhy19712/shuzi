95e98b805e3b3136629640bd53016dba6166ae72
zhangchuan <993090861@qq.com>
2018-03-13 11:29:16 +0800

    规章制度

--- public/datatables/safety/safety_education.php
+++ public/datatables/safety/safety_education.php
@@ -30,14 +30,14 @@ $primaryKey = 'id';
 // parameter represents the DataTables column identifier. In this case simple
 // indexes
 $columns = array(//定义数据库中查看的字段与表格中的哪一列相对应
-    array( 'db' => 'id',  'dt' => 0 ),
-    array( 'db' => 'content',  'dt' => 1 ),
-    array( 'db' => 'edu_time',  'dt' => 2 ),
-    array( 'db' => 'address',  'dt' => 3 ),
-    array( 'db' => 'lecturer',  'dt' => 4 ),
-    array( 'db' => 'trainee',  'dt' => 5 ),
-    array( 'db' => 'num',  'dt' => 6 ),
-    array( 'db' => 'remark',  'dt' => 7)
+    array( 'db' => 'id',  'dt' => 1 ),
+    array( 'db' => 'content',  'dt' => 2 ),
+    array( 'db' => 'edu_time',  'dt' => 3 ),
+    array( 'db' => 'address',  'dt' => 4 ),
+    array( 'db' => 'lecturer',  'dt' => 5 ),
+    array( 'db' => 'trainee',  'dt' => 6 ),
+    array( 'db' => 'num',  'dt' => 7 ),
+    array( 'db' => 'remark',  'dt' => 8)
 );
 
 
--- public/datatables/safety/safety_rules.php
+++ public/datatables/safety/safety_rules.php
@@ -30,15 +30,15 @@ $primaryKey = 'id';
 // parameter represents the DataTables column identifier. In this case simple
 // indexes
 $columns = array(//定义数据库中查看的字段与表格中的哪一列相对应
-    array( 'db' => 'id',  'dt' => 0 ),
-    array( 'db' => 'number',  'dt' => 1 ),
-    array( 'db' => 'rul_name',  'dt' => 2 ),
-    array( 'db' => 'go_date',  'dt' => 3 ),
-    array( 'db' => 'standard',  'dt' => 4 ),
-    array( 'db' => 'evaluation',  'dt' => 5 ),
-    array( 'db' => 'rul_user',  'dt' => 6 ),
-    array( 'db' => 'rul_date',  'dt' => 7 ),
-    array( 'db' => 'remark',  'dt' => 8)
+    array( 'db' => 'id',  'dt' => 1 ),
+    array( 'db' => 'number',  'dt' => 2 ),
+    array( 'db' => 'rul_name',  'dt' => 3 ),
+    array( 'db' => 'go_date',  'dt' => 4 ),
+    array( 'db' => 'standard',  'dt' => 5 ),
+    array( 'db' => 'evaluation',  'dt' => 6 ),
+    array( 'db' => 'rul_user',  'dt' => 7 ),
+    array( 'db' => 'rul_date',  'dt' => 8 ),
+    array( 'db' => 'remark',  'dt' => 9)
 );
 
 
