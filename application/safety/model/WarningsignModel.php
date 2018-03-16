a41118534e8d4e731681422b4d0dbaf05e22114d
13522390376 <13522390376@163.com>
2018-03-16 11:57:58 +0800

    Merge branch 'master' of https://github.com/zhy19712/shuzi into archive
                                                # Conflicts:
                                                #	application/safety/controller/Upload.php

--- application/safety/model/WarningsignModel.php
+++ application/safety/model/WarningsignModel.php
@@ -45,6 +45,71 @@ class WarningsignModel extends Model
 
     }
 
+    /*
+     * 添加新的警示标志
+    */
+    public function insertWarningsign($param)
+    {
+        try{
+            $result = $this->allowField(true)->save($param);
+            if(false === $result){
+                return ['code' => -1, 'data' => '', 'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'data' => '', 'msg' => '添加成功'];
+            }
+        }catch( PDOException $e){
+            return ['code' => -2, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    /*
+     * 编辑警示标志
+    */
+    public function editWarningsign($param)
+    {
+        try{
+            $result =  $this->allowField(true)->save($param, ['id' => $param['id']]);
+            if(false === $result){
+                return ['code' => 0, 'data' => '', 'msg' => $this->getError()];
+            }else{
+                return ['code' => 1, 'data' => '', 'msg' => '编辑成功'];
+            }
+        }catch( PDOException $e){
+            return ['code' => 0, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    /*
+     * 删除警示标志
+    */
+    public function delWarningsign($id)
+    {
+        try{
+            $this->where('id', $id)->delete();
+            return ['code' => 1, 'data' => '', 'msg' => '删除成功'];
+
+        }catch( PDOException $e){
+            return ['code' => 0, 'data' => '', 'msg' => $e->getMessage()];
+        }
+    }
+
+    /*
+     * 获取一条警示标志记录
+    */
+    /**
+     * @param $id
+     * @return array|false|\PDOStatement|string|Model
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getOne($id)
+    {
+        return $this->where('id', $id)->find();
+    }
+
+
+
 
 
 }
