35191c5726f904fbd0f51f3b01dd6b7abcd8b705
13522390376 <13522390376@163.com>
2018-03-16 10:07:25 +0800

    Merge branch 'master' of https://github.com/zhy19712/shuzi into archive
                                                # Conflicts:
                                                #	application/safety/controller/Upload.php

--- application/safety/controller/Warningsign.php
+++ application/safety/controller/Warningsign.php
@@ -5,3 +5,21 @@
  * Date: 2018/3/15
  * Time: 19:09
  */
+//警示标志
+namespace app\safety\controller;
+
+use app\admin\controller\Base;
+use think\Db;
+use think\Loader;
+use app\safety\model\AccidentfileModel;
+
+class Warningsign extends Base
+{
+    /*
+     * 获取一条事故报告信息
+    */
+    public function index()
+    {
+        return $this->fetch();
+    }
+}
