a587fed07fe7b67eb45dd44176a160f6aef9a16c
13522390376 <13522390376@163.com>
2018-03-20 18:23:38 +0800

    Merge branch 'master' of https://github.com/zhy19712/shuzi into archive
                                                # Conflicts:
                                                #	application/safety/controller/Upload.php

--- application/safety/controller/Boardinginner.php
+++ application/safety/controller/Boardinginner.php
@@ -60,10 +60,10 @@ class Boardinginner extends Base
      */
     public function boardinginnerDel()
     {
-        $boardingequipment = new BoardingequipmentModel();
+        $boarding = new BoardinginnerModel();
         if(request()->isAjax()) {
             $param = input('post.');
-            $data = $boardingequipment->getOne($param['id']);
+            $data = $boarding->getOne($param['id']);
             $path = $data['path'];
             $pdf_path = './uploads/temp/' . basename($path) . '.pdf';
             if(file_exists($path)){
@@ -72,8 +72,140 @@ class Boardinginner extends Base
             if(file_exists($pdf_path)){
                 unlink($pdf_path); //删除生成的预览pdf
             }
-            $flag = $boardingequipment->delBoardingequipment($param['id']);
+            $flag = $boarding->delBoardinginner($param['id']);
             return json(['code' => $flag['code'], 'data' => $flag['data'], 'msg' => $flag['msg']]);
         }
+    }
+
+    /**
+     * 批量导出
+     * @return \think\response\Json
+     * @throws \PHPExcel_Exception
+     * @throws \PHPExcel_Reader_Exception
+     * @throws \PHPExcel_Writer_Exception
+     */
+    public function exportExcel()
+    {
+        if(request()->isAjax()){
+            return json(['code'=>1]);
+        }
+        $boarding = new BoardinginnerModel();
+        $idArr = input('param.idarr/a');
+        if($idArr['0'] == "all")
+        {
+            $idArr = $boarding ->getallid();
+        }
+        $name = '登高工器具'.date('Y-m-d H:i:s'); // 导出的文件名
+
+        $list = $boarding->getList($idArr);
+        header("Content-type:text/html;charset=utf-8");
+        Loader::import('PHPExcel\Classes\PHPExcel', EXTEND_PATH);
+        //实例化
+        $objPHPExcel = new \PHPExcel();
+        /*右键属性所显示的信息*/
+        $objPHPExcel->getProperties()->setCreator("zxf")  //作者
+        ->setLastModifiedBy("zxf")  //最后一次保存者
+        ->setTitle('数据EXCEL导出')  //标题
+        ->setSubject('数据EXCEL导出') //主题
+        ->setDescription('导出数据')  //描述
+        ->setKeywords("excel")   //标记
+        ->setCategory("result file");  //类别
+        //设置当前的表格
+        $objPHPExcel->setActiveSheetIndex(0);
+        // 设置表格第一行显示内容
+        $objPHPExcel->getActiveSheet()
+            ->setCellValue('A1', '序号')
+            ->setCellValue('B1', '工器具名称')
+            ->setCellValue('C1', '规格型号')
+            ->setCellValue('D1', '数量')
+            ->setCellValue('E1', '批次')
+            ->setCellValue('F1', '生产厂家')
+            ->setCellValue('G1', '出厂日期')
+            ->setCellValue('H1', '定检周期')
+            ->setCellValue('I1', '首检日期')
+            ->setCellValue('J1', '使用位置')
+            ->setCellValue('K1', '备注');
+        $key = 1;
+        /*以下就是对处理Excel里的数据，横着取数据*/
+        foreach($list as $v){
+            //设置循环从第二行开始
+            $key++;
+            $objPHPExcel->getActiveSheet()
+                //Excel的第A列，name是你查出数组的键值字段，下面以此类推
+                ->setCellValue('A'.$key, $v['id'])
+                ->setCellValue('B'.$key, $v['tool_name'])
+                ->setCellValue('C'.$key, $v['type_model'])
+                ->setCellValue('D'.$key, $v['number'])
+                ->setCellValue('E'.$key, $v['batch'])
+                ->setCellValue('F'.$key, $v['manufacture'])
+                ->setCellValue('G'.$key, $v['date_product'])
+                ->setCellValue('H'.$key, $v['check_round'])
+                ->setCellValue('I'.$key, $v['first_check_date'])
+                ->setCellValue('J'.$key, $v['use_position'])
+                ->setCellValue('K'.$key, $v['remark']);
+        }
+        //设置当前的表格
+        $objPHPExcel->setActiveSheetIndex(0);
+        ob_end_clean();  //清除缓冲区,避免乱码
+        header('Content-Type: application/vnd.ms-excel'); //文件类型
+        header('Content-Disposition: attachment;filename="'.$name.'.xls"'); //文件名
+        header('Cache-Control: max-age=0');
+        header('Content-Type: text/html; charset=utf-8'); //编码
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');  //excel 2003
+        $objWriter->save('php://output');
+        exit;
+    }
+
+    /**
+     * 导出模板
+     * @return \think\response\Json
+     * @throws \PHPExcel_Exception
+     * @throws \PHPExcel_Reader_Exception
+     * @throws \PHPExcel_Writer_Exception
+     */
+    public function exportExcelTemplete()
+    {
+        if(request()->isAjax()){
+            return json(['code'=>1]);
+        }
+        $name = input('param.name');
+        $newName = '登高工器具 - '.$name.date('Y-m-d H:i:s'); // 导出的文件名
+        header("Content-type:text/html;charset=utf-8");
+        Loader::import('PHPExcel\Classes\PHPExcel', EXTEND_PATH);
+        //实例化
+        $objPHPExcel = new \PHPExcel();
+        /*右键属性所显示的信息*/
+        $objPHPExcel->getProperties()->setCreator("zxf")  //作者
+        ->setLastModifiedBy("zxf")  //最后一次保存者
+        ->setTitle('数据EXCEL导出')  //标题
+        ->setSubject('数据EXCEL导出') //主题
+        ->setDescription('导出数据')  //描述
+        ->setKeywords("excel")   //标记
+        ->setCategory("result file");  //类别
+        //设置当前的表格
+        $objPHPExcel->setActiveSheetIndex(0);
+        // 设置表格第一行显示内容
+        $objPHPExcel->getActiveSheet()
+            ->setCellValue('A1', '序号')
+            ->setCellValue('B1', '工器具名称')
+            ->setCellValue('C1', '规格型号')
+            ->setCellValue('D1', '数量')
+            ->setCellValue('E1', '批次')
+            ->setCellValue('F1', '生产厂家')
+            ->setCellValue('G1', '出厂日期')
+            ->setCellValue('H1', '定检周期')
+            ->setCellValue('I1', '首检日期')
+            ->setCellValue('J1', '使用位置')
+            ->setCellValue('K1', '备注');
+        //设置当前的表格
+        $objPHPExcel->setActiveSheetIndex(0);
+        ob_end_clean();  //清除缓冲区,避免乱码
+        header('Content-Type: application/vnd.ms-excel'); //文件类型
+        header('Content-Disposition: attachment;filename="'.$newName.'.xls"'); //文件名
+        header('Cache-Control: max-age=0');
+        header('Content-Type: text/html; charset=utf-8'); //编码
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');  //excel 2003
+        $objWriter->save('php://output');
+        exit;
     }
 }
