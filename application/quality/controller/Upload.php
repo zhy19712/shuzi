e87a442771ef6a9866d411eb8e4b38db2454ed7a
huwtao <hutaosir@163.com>
2018-05-08 21:01:22 +0800

    commit history

--- application/quality/controller/Upload.php
+++ application/quality/controller/Upload.php
@@ -386,22 +386,30 @@ class Upload extends Base
         $mater = new MaterialfileModel();
         // 前台提交的数据
         $major_key = request()->param('major_key'); // 可选 文件自增编号 新增时 可以不必传 注意 修改的时候一定要传
-
         // 数组
-        $entrustment_number = request()->param('entrustment_number/a'); //  委托编号
-        dump($entrustment_number);
-        halt(input());
-        $report_number = request()->param('report_number/a'); //  报告编号
-        $approach_detection_time = request()->param('approach_detection_time/a'); //  进场检测时间/成型日期
-        $using_position = request()->param('using_position/a'); //  使用部位/工程部位/检测部位
-        $manufacturer = request()->param('manufacturer/a'); //  生产厂家
-        $specifications = request()->param('specifications/a'); //  品种/标号/规格/等级/种类
-        $lot_number = request()->param('lot_number/a'); //  批号/炉号/型号/桩号/母材批号
-        $number_of_delegates = request()->param('number_of_delegates/a'); // 代表数量/进场数量
-        $conclusion = request()->param('conclusion/a'); // 结论
+        $entrustment_number_1 = request()->param('entrustment_number'); //  委托编号
+        $entrustment_number = explode(',',$entrustment_number_1);
+        $report_number_1 = request()->param('report_number'); //  报告编号
+        $report_number = explode(',',$report_number_1);
+        $approach_detection_time_1 = request()->param('approach_detection_time'); //  进场检测时间/成型日期
+        $approach_detection_time = explode(',',$approach_detection_time_1);
+        $using_position_1 = request()->param('using_position'); //  使用部位/工程部位/检测部位
+        $using_position = explode(',',$using_position_1);
+        $manufacturer_1 = request()->param('manufacturer'); //  生产厂家
+        $manufacturer = explode(',',$manufacturer_1);
+        $specifications_1 = request()->param('specifications'); //  品种/标号/规格/等级/种类
+        $specifications = explode(',',$specifications_1);
+        $lot_number_1 = request()->param('lot_number'); //  批号/炉号/型号/桩号/母材批号
+        $lot_number = explode(',',$lot_number_1);
+        $number_of_delegates_1 = request()->param('number_of_delegates'); // 代表数量/进场数量
+        $number_of_delegates = explode(',',$number_of_delegates_1);
+        $conclusion_1 = request()->param('conclusion'); // 结论
+        $conclusion = explode(',',$conclusion_1);
 
-        $kind = request()->param('kind/a'); // 样品名称/检测项目
-        $bids = request()->param('bids/a'); //  标段
+        $kind_1 = request()->param('kind'); // 样品名称/检测项目
+        $kind = explode(',',$kind_1);
+        $bids_1 = request()->param('bids'); //  标段
+        $bids = explode(',',$bids_1);
 
         // 单个值
         $broken_date = request()->param('broken_date'); // 破型日期
@@ -415,73 +423,56 @@ class Upload extends Base
         $order_number = request()->param('order_number'); //  序号
 
         $data = [];
-        // 水泥,粉煤灰,钢筋,粗骨料,细骨料,减水剂,速凝剂,引气剂,微膨胀剂,纤维,钢止水,橡胶止水,PVC止水,钢绞线,土工布
-        for($i = 0; $i < 15;$i++){
-            // 委托编号、报告编号、进场检测时间、使用部位、生产厂家
-            $data[$i]['entrustment_number'] = $entrustment_number[$i];
-            $data[$i]['report_number'] = $report_number[$i];
+        $en = $ma = $sp = $lot = $del = 0;
+        for($i = 0; $i < 20;$i++){
+            // 委托编号、报告编号、进场检测时间
+            if($i != 16){
+                $data[$i]['entrustment_number'] = $entrustment_number[$en];
+                $data[$i]['report_number'] = $report_number[$en];
+                $en++;
+            }
             $data[$i]['approach_detection_time'] = $approach_detection_time[$i];
-            $data[$i]['using_position'] = $using_position[$i];
-            $data[$i]['manufacturer'] = $manufacturer[$i];
-            if(in_array($i,[0,1,3,4,5,6,7,8,9,10,11,12,13])){
-                $data[$i]['manufacturer'] = $specifications[$i]; // 品种/标号/规格/等级/种类
+            $data[$i]['using_position'] = $using_position[$i]; // 使用部位
+            if($i < 15){
+                $data[$i]['manufacturer'] = $manufacturer[$ma]; // 生产厂家
+                $ma++;
             }
-            if(in_array($i,[0,1,2,5,6,7,8,9,10,11,12,13])){
-                $data[$i]['lot_number'] = $lot_number[$i]; // 批号/炉号/型号/桩号/母材批号
+            if(!in_array($i,[2,14,17,18,19])){
+                $data[$i]['specifications'] = $specifications[$sp]; // 品种/标号/规格/等级/种类
+                $sp++;
             }
-            $data[$i]['number_of_delegates'] = $number_of_delegates[$i]; // 代表数量/进场数量
-            $data[$i]['$conclusion'] = $conclusion[$i]; // 结论
+            if(!in_array($i,[3,4,16,18,19])){
+                $data[$i]['lot_number'] = $lot_number[$lot]; // 批号/炉号/型号/桩号/母材批号
+                $lot++;
+            }
+            if(!in_array($i,[15,17,18,19])){
+                $data[$i]['number_of_delegates'] = $number_of_delegates[$del]; // 代表数量/进场数量
+                $del++;
+            }
+            $data[$i]['conclusion'] = $conclusion[$i]; // 结论
         }
-        //  混凝土 ==>委托编号、报告编号、成型日期、破型日期、标段、工程部位、桩号、高程、种类、设计强度等级、龄期(d)、抗压强度(Mpa)、结论
-        $data[14]['entrustment_number'] = $entrustment_number[14];
-        $data[14]['report_number'] = $report_number[14];
-        $data[14]['approach_detection_time'] = $approach_detection_time[14];
-        $data[14]['broken_date'] = $broken_date;
-        $data[14]['bids'] = $bids[0];
-        $data[14]['using_position'] = $using_position[14];
-        $data[14]['lot_number'] = $lot_number[14];
-        $data[14]['altitude'] = $altitude;
-        $data[14]['specifications'] = $specifications[14];
-        $data[14]['design_strength_grade'] = $design_strength_grade;
-        $data[14]['age'] = $age;
-        $data[14]['compression_strength'] = $compression_strength;
-        $data[14]['conclusion'] = $conclusion[14];
 
+        //  混凝土 ==>委托编号、报告编号、成型日期、破型日期、标段、工程部位、桩号、高程、种类、设计强度等级、龄期(d)、抗压强度(Mpa)、结论
+        $data[15]['broken_date'] = $broken_date;
+        $data[15]['bids'] = $bids[0];
+        $data[15]['altitude'] = $altitude;
+        $data[15]['design_strength_grade'] = $design_strength_grade;
+        $data[15]['age'] = $age;
+        $data[15]['compression_strength'] = $compression_strength;
         // 钢筋接头==> 检测日期、标段、工程部位、品种规格、钢筋直径、代表数量(个)、结论
-        $data[15]['approach_detection_time'] = $approach_detection_time[15];
-        $data[15]['bids'] = $bids[1];
-        $data[15]['using_position'] = $using_position[15];
-        $data[15]['specifications'] = $specifications[15];
-        $data[15]['bar_diameter'] = $bar_diameter;
-        $data[15]['number_of_delegates'] = $number_of_delegates[14];
-        $data[15]['conclusion'] = $conclusion[15];
+        $data[16]['bids'] = $bids[1];
+        $data[16]['bar_diameter'] = $bar_diameter;
         // 止水材料接头==> 委托编号、报告编号、检测日期、标段、工程部位、母材批号、状态、焊接或连接方式、结论
-        $data[16]['entrustment_number'] = $entrustment_number[15];
-        $data[16]['report_number'] = $report_number[15];
-        $data[16]['approach_detection_time'] = $approach_detection_time[16];
-        $data[16]['bids'] = $bids[2];
-        $data[16]['using_position'] = $using_position[16];
-        $data[16]['lot_number'] = $lot_number[15];
-        $data[16]['status'] = $status;
-        $data[16]['welding'] = $welding;
-        $data[16]['conclusion'] = $conclusion[16];
+        $data[17]['bids'] = $bids[2];
+        $data[17]['status'] = $status;
+        $data[17]['welding'] = $welding;
         // 压实度==> 委托编号、报告编号、检测日期、标段、检测部位、样品名称、结论
-        $data[17]['entrustment_number'] = $entrustment_number[16];
-        $data[17]['report_number'] = $report_number[16];
-        $data[17]['approach_detection_time'] = $approach_detection_time[17];
-        $data[17]['bids'] = $bids[3];
-        $data[17]['using_position'] = $using_position[17];
-        $data[17]['kind'] = $kind[0];
-        $data[17]['conclusion'] = $conclusion[17];
+        $data[18]['bids'] = $bids[3];
+        $data[18]['kind'] = $kind[0];
         // 地基承载力==> 序号、委托编号、报告编号、检测日期、标段、检测部位、检测项目、结论
-        $data[18]['order_number'] = $order_number;
-        $data[18]['entrustment_number'] = $entrustment_number[17];
-        $data[18]['report_number'] = $report_number[17];
-        $data[18]['approach_detection_time'] = $approach_detection_time[18];
-        $data[18]['bids'] = $bids[4];
-        $data[18]['using_position'] = $using_position[18];
-        $data[18]['kind'] = $kind[1];
-        $data[18]['conclusion'] = $conclusion[1];
+        $data[19]['order_number'] = $order_number;
+        $data[19]['bids'] = $bids[4];
+        $data[19]['kind'] = $kind[1];
 
 
         // 系统自动生成的数据
--- application/quality/model/MaterialfileModel.php
+++ application/quality/model/MaterialfileModel.php
@@ -15,6 +15,8 @@ use think\Model;
 class MaterialfileModel extends Model
 {
     protected $name = 'project_test_data';
+    //自动写入创建、更新时间 insertGetId和update方法中无效，只能用于save方法
+    protected $autoWriteTimestamp = true;
 
     public function insertMater($param)
     {
@@ -63,6 +65,10 @@ class MaterialfileModel extends Model
     public function delMater($id)
     {
         try{
+            $data = $this->getOne($id);
+            if(file_exists($data['path'])){
+                unlink($data['path']); //删除文件
+            }
             $this->where('id', $id)->whereOr('relevance_id',$id)->delete();
             return ['code' => 1, 'data' => '', 'msg' => '删除成功'];
 
@@ -73,7 +79,50 @@ class MaterialfileModel extends Model
 
     public function getAllOne($id)
     {
-        $data = $this->where(['id'=>$id])->whereOr(['relevance_id'=>$id])->select();
+        $data = [];
+        $da = $this->where(['id'=>$id])->whereOr(['relevance_id'=>$id])->order('id asc')->select();
+        for($i = 0; $i < 20;$i++){
+            if($i != 16){
+                $data[$i]['entrustment_number'] = $da[$i]['entrustment_number'];
+                $data[$i]['report_number'] = $da[$i]['report_number'];
+            }
+            $data[$i]['approach_detection_time'] = $da[$i]['approach_detection_time'];
+            $data[$i]['using_position'] = $da[$i]['using_position']; // 使用部位
+            if($i < 15){
+                $data[$i]['manufacturer'] = $da[$i]['manufacturer']; // 生产厂家
+            }
+            if(!in_array($i,[2,14,17,18,19])){
+                $data[$i]['specifications'] = $da[$i]['specifications']; // 品种/标号/规格/等级/种类
+            }
+            if(!in_array($i,[3,4,16,18,19])){
+                $data[$i]['lot_number'] = $da[$i]['lot_number']; // 批号/炉号/型号/桩号/母材批号
+            }
+            if(!in_array($i,[15,17,18,19])){
+                $data[$i]['number_of_delegates'] = $da[$i]['number_of_delegates']; // 代表数量/进场数量
+            }
+            $data[$i]['conclusion'] = $da[$i]['conclusion']; // 结论
+        }
+        //  混凝土 ==>委托编号、报告编号、成型日期、破型日期、标段、工程部位、桩号、高程、种类、设计强度等级、龄期(d)、抗压强度(Mpa)、结论
+        $data[15]['broken_date'] = $da[15]['broken_date'];
+        $data[15]['bids'] = $da[15]['bids'];
+        $data[15]['altitude'] = $da[15]['altitude'];
+        $data[15]['design_strength_grade'] = $da[15]['design_strength_grade'];
+        $data[15]['age'] = $da[15]['age'];
+        $data[15]['compression_strength'] = $da[15]['compression_strength'];
+        // 钢筋接头==> 检测日期、标段、工程部位、品种规格、钢筋直径、代表数量(个)、结论
+        $data[16]['bids'] = $da[16]['bids'];
+        $data[16]['bar_diameter'] = $da[16]['bar_diameter'];
+        // 止水材料接头==> 委托编号、报告编号、检测日期、标段、工程部位、母材批号、状态、焊接或连接方式、结论
+        $data[17]['bids'] = $da[17]['bids'];
+        $data[17]['status'] = $da[17]['status'];
+        $data[17]['welding'] = $da[17]['welding'];
+        // 压实度==> 委托编号、报告编号、检测日期、标段、检测部位、样品名称、结论
+        $data[18]['bids'] = $da[18]['bids'];
+        $data[18]['kind'] = $da[18]['kind'];
+        // 地基承载力==> 序号、委托编号、报告编号、检测日期、标段、检测部位、检测项目、结论
+        $data[19]['order_number'] = $da[19]['order_number'];
+        $data[19]['bids'] = $da[19]['bids'];
+        $data[19]['kind'] = $da[19]['kind'];
         return $data;
     }
 
