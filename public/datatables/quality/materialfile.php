4bdf13e4a35dbdea6e9d4be650db092d0a76ad41
huwtao <hutaosir@163.com>
2018-05-14 19:30:03 +0800

    commit history

--- public/datatables/quality/materialfile.php
+++ public/datatables/quality/materialfile.php
@@ -51,10 +51,10 @@ $columns = array(//定义数据库中查看的字段与表格中的哪一列相�
 
 require( '../ssp.class.php' );
 
-if(!empty($_POST["uid"]))
+if(!empty($_GET["uid"]))
 {
     $u = new \app\quality\controller\UnitEngineering();
-    $test_data_id = $u->datumNumber($_POST["uid"],1);
+    $test_data_id = $u->datumNumber($_GET["uid"],1);
     echo json_encode(
         SSP::complex( $_GET, $sql_details, $table, $primaryKey, $columns, null, "relevance_id = '0' and id in '$test_data_id'" )
     );
--- public/datatables/quality/surveydata.php
+++ public/datatables/quality/surveydata.php
@@ -52,10 +52,10 @@ $columns = array(//定义数据库中查看的字段与表格中的哪一列相�
 require( '../ssp.class.php' );
 
 
-if(!empty($_POST["uid"]))
+if(!empty($_GET["uid"]))
 {
     $u = new \app\quality\controller\UnitEngineering();
-    $survey_data_id = $u->datumNumber($_POST["uid"],2);
+    $survey_data_id = $u->datumNumber($_GET["uid"],2);
     echo json_encode(
         SSP::complex( $_GET, $sql_details, $table, $primaryKey, $columns, null, " id in '$survey_data_id'" )
     );
